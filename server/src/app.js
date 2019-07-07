3407c40c5a020874c5b47f716d71848e00a7a3a3
YeonwooSung <neos960518@gmail.com>
2019-07-08 00:51:27 +0900

    implement server skeleton

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//import required libraries
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let app = express();
+
+const bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+
+
+//basic set ups
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
